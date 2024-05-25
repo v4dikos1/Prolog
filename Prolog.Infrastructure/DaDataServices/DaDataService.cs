7af2b6f1b714f8d10022306c6d8f495764c1b53d
v4dikos1 <vadim.iskhakov.02@outlook.com>
2024-05-25 13:59:35 +0700

    фикс

--- Prolog.Infrastructure/DaDataServices/DaDataService.cs
+++ Prolog.Infrastructure/DaDataServices/DaDataService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Prolog.Abstractions.CommonModels.DaDataService;
 using Prolog.Abstractions.CommonModels.DaDataService.Models.Query;
 using Prolog.Abstractions.CommonModels.DaDataService.Models.Response;
@@ -7,7 +8,7 @@ using Prolog.Infrastructure.HttpClients;
 
 namespace Prolog.Infrastructure.DaDataServices;
 
-public class DaDataService(IConfiguration configuration) : IDaDataService
+public class DaDataService(IConfiguration configuration, ILogger<DaDataHttpClient> logger) : IDaDataService
 {
     private readonly string _apiKey = configuration.GetSection("DaDataService:ApiKey").Value!;
     private readonly string _daDataServiceUrl = configuration.GetSection("DaDataService:BaseUrl").Value!;
@@ -15,21 +16,21 @@ public class DaDataService(IConfiguration configuration) : IDaDataService
     public async Task<IEnumerable<SuggestionResponseModel>> GetListSuggestionAddressByQuery(
         AddressQueryModel queryModel)
     {
-        using var client = new DaDataHttpClient(_apiKey, _daDataServiceUrl);
+        using var client = new DaDataHttpClient(_apiKey, _daDataServiceUrl, logger);
         var suggestionList = await client.GetAddress(queryModel);
         return suggestionList.Suggestions;
     }
 
     public async Task<SuggestionResponseModel> GetAddressByHouseFiasId(string fiasHouseId)
     {
-        using var client = new DaDataHttpClient(_apiKey, _daDataServiceUrl);
+        using var client = new DaDataHttpClient(_apiKey, _daDataServiceUrl, logger);
         var suggestionList = await client.GetAddressByHouseFiasId(fiasHouseId);
         return suggestionList.Suggestions.FirstOrDefault()!;
     }
 
     public async Task<CoordinatesResponseModel> GetCoordinatesByAddress(string address)
     {
-        using var client = new DaDataHttpClient(_apiKey, _daDataServiceUrl);
+        using var client = new DaDataHttpClient(_apiKey, _daDataServiceUrl, logger);
         {
             var coordinates = await client.GetCoordinatesByAddress(address);
             return coordinates;
