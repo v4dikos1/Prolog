dc109acb39d3ff4ae09433ce043eefd60dd3eb07
v4dikos1 <vadim.iskhakov.02@outlook.com>
2024-05-29 21:00:29 +0700

    fix

--- Prolog.Api/Program.cs
+++ Prolog.Api/Program.cs
@@ -64,7 +64,7 @@ try
                     .AllowAnyMethod()
                     .WithExposedHeaders("*");
             });
-
+         
         options.AddPolicy("SignalR",
             build =>
             {
